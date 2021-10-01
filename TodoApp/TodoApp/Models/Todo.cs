8ff69f162d9ff289e48b0866df2f18c50fdbd837
bardabarda228 <bardabarda228@gmail.com>
2021-10-01 14:12:52 +0500

    Добавьте файлы проекта.

---
+++ TodoApp/TodoApp/Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TodoApp.Models
+{
+    public class Todo
+    {
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public bool IsDone
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ TodoApp/TodoApp/ViewModels/ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using TodoApp.Models;
+using Xamarin.Forms;
+
+namespace TodoApp.ViewModels
+{
+    public class ViewModel : BindableObject
+    {
+        public ObservableCollection<Todo> Todos { get; set; }
+
+        public ViewModel()
+        {
+            Todos = new ObservableCollection<Todo>()
+            {
+                new Todo {Title = "Title1"},
+                new Todo {Title = "Title2"},
+                new Todo {Title = "Title3"}
+            };
+        }
+
+        private string _note;
+
+        public string Note
+        {
+            get => _note;
+            set
+            {
+                if (_note != value)
+                {
+                    _note = value;
+                    OnPropertyChanged("Note");
+                }
+            }
+        }
+    }
+}
